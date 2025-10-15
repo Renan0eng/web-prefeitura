7c86fe952696f367054dd9232f21571f31a05b62
Renan Nardi <renan.nardi.dev@gmail.com>
2025-10-15 00:21:47 -0300

    prisma

--- src/hooks/user-data.tsx
+++ src/hooks/user-data.tsx
@@ -1,36 +1,26 @@
 "use client"
-import { Menu_Acesso, Prisma } from "@prisma/client"
 import React, { createContext, ReactNode, useContext, useState } from "react"
 
 
-export type UserWithNivelAcesso = Prisma.UserGetPayload<{
-  include: {
-    nivel_acesso: {
-      include: {
-        menus: true
-      }
-    }
-  }
-}>
 
 type UserContextType = {
-  user: UserWithNivelAcesso | null
-  setUser: React.Dispatch<React.SetStateAction<UserWithNivelAcesso | null>>
-  getNivelAcesso: (slug: string) => Menu_Acesso
-  getAllNivelAcesso: () => Menu_Acesso[]
+  user: any | null
+  setUser: React.Dispatch<React.SetStateAction<any | null>>
+  getNivelAcesso: (slug: string) => any
+  getAllNivelAcesso: () => any[]
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined)
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState<UserWithNivelAcesso | null>(null)
+  const [user, setUser] = useState<any | null>(null)
 
   const getNivelAcesso = (slug: string) => {
-    return user?.nivel_acesso?.menus.find((e) => e.slug === slug || e.slug === "") as Menu_Acesso
+    return user?.nivel_acesso?.menus.find((e: any) => e.slug === slug || e.slug === "")  
   }
 
   const getAllNivelAcesso = () => {
-    return user?.nivel_acesso?.menus as Menu_Acesso[]
+    return user?.nivel_acesso?.menus 
   }
 
   return <UserContext.Provider value={{ user, setUser, getNivelAcesso, getAllNivelAcesso }}>{children}</UserContext.Provider>
